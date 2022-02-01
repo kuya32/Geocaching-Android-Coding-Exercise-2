c25b54a57026f7d6c3655b6fc2419ddee249764c
Marchael Acode <m.acode@outlook.com>
2022-02-01 15:37:44 -0800

    Implemented splash screen with new api.

--- app/src/main/java/com/github/kuya32/geocachingandroidcodingexercise2/MainActivity.kt
+++ app/src/main/java/com/github/kuya32/geocachingandroidcodingexercise2/MainActivity.kt
@@ -3,24 +3,38 @@ package com.github.kuya32.geocachingandroidcodingexercise2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.kuya32.geocachingandroidcodingexercise2.ui.theme.GeocachingAndroidCodingExercise2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             GeocachingAndroidCodingExercise2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    contentAlignment = Alignment.Center
                 ) {
                     Greeting("Android")
                 }
